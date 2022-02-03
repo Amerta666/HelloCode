b4837f7cd26d2e9ed15882b1f5b59c9e032b185f
SHOCK <beponuka666@gmail.com>
2022-02-03 19:04:21 +0500

    Add Tasks Task002

--- Tasks/Task002/Program.cs
+++ Tasks/Task002/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// 2. Даны два числа. Показать большее и меньшее число
+string sa = Console.ReadLine();
+string sb = Console.ReadLine();
+int first_number = int.Parse(sa);
+int second_number = int.Parse(sb);
+if (first_number > second_number) 
+{ 
+    Console.Write(first_number); 
+    Console.WriteLine(" является большим числом");
+    Console.Write(second_number); 
+    Console.Write(" является меньшим");
+}
+else 
+{
+    Console.Write(second_number); 
+    Console.WriteLine(" является большим числом");
+    Console.Write(first_number); 
+    Console.Write(" является меньшим");
+}
