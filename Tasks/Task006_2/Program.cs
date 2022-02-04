c774deed7d4a59d8ae1a2c118e13cf5f4854cc11
SHOCK <beponuka666@gmail.com>
2022-02-04 22:00:36 +0500

    Added Task006_2

---
+++ Tasks/Task006_2/Program.cs
@@ -0,0 +1,18 @@
+﻿// 6. Выяснить является ли число чётным (Альтернативный вариант)
+// using static System.Console;
+// - после этого можно писать "WriteLine", а не "Console.WriteLine" в коде и.т.д 
+
+Console.WriteLine("Введите число"); 
+string num = Console.ReadLine();  // int Number = int.Parse(Console.ReadLine()); 
+int Number = int.Parse(num);
+
+if (Number % 2 == 0)
+{
+    Console.Write(Number);
+    Console.Write(" является чётным числом");
+    return;
+}    
+
+Console.Write(Number);
+Console.WriteLine(" является нечётным числом");
+
