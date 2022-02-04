ca532092eb41a72891b35ee041efa1f98e70b75d
SHOCK <beponuka666@gmail.com>
2022-02-04 21:51:30 +0500

    Added Task006

---
+++ Tasks/Task006/Program.cs
@@ -0,0 +1,15 @@
+﻿// 6. Выяснить является ли число чётным
+Console.WriteLine("Введите число");
+string num = Console.ReadLine();  // int Number = int.Parse(Console.ReadLine()); 
+int Number = int.Parse(num);
+if (Number % 2 == 0)
+{
+    Console.Write(Number);
+    Console.Write(" является чётным числом");
+}    
+else 
+{   
+    Console.Write(Number);
+    Console.WriteLine(" является нечётным числом");
+}
+
