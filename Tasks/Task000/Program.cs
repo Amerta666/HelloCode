a81aec8ab40d08fb6c6f2cb39bc0bc6b0d7dcd8a
SHOCK <beponuka666@gmail.com>
2022-02-04 21:12:20 +0500

    Added text

--- Tasks/Task000/Program.cs
+++ Tasks/Task000/Program.cs
@@ -1,5 +1,5 @@
 ﻿// 0. Вывести квадрат числа
-
+Console.WriteLine("Введите число, которое нужно возвести в квадрат");
 string ss = Console.ReadLine();
 int number = int.Parse(ss);
 Console.WriteLine(number * number);
