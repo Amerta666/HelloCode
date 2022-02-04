d2afd5d8947e9352ada7cfbeec4e629afca9da20
SHOCK <beponuka666@gmail.com>
2022-02-04 21:34:36 +0500

    Code written

--- Tasks/Task003/Program.cs
+++ Tasks/Task003/Program.cs
@@ -1,6 +1,36 @@
 ﻿// 3. По заданному номеру дня недели вывести его название
 Console.WriteLine("Введите номер дня недели");
-string numberday of the week
-
-
-Console.WriteLine("Hello, World!");
+string NW = Console.ReadLine();
+int NumberDayWeek = int.Parse(NW);
+if (NumberDayWeek == 1) 
+{   
+    Console.WriteLine("Понедельник");
+}
+if (NumberDayWeek == 2) 
+{   
+    Console.WriteLine("Вторник");
+}
+if (NumberDayWeek == 3) 
+{   
+    Console.WriteLine("Среда");
+}
+if (NumberDayWeek == 4) 
+{   
+    Console.WriteLine("Четверг");
+}
+if (NumberDayWeek == 5) 
+{   
+    Console.WriteLine("Пятница");
+}
+if (NumberDayWeek == 6) 
+{   
+    Console.WriteLine("Суббота");
+}
+if (NumberDayWeek == 7) 
+{   
+    Console.WriteLine("Воскресенье");
+}
+if (NumberDayWeek > 7) 
+{   
+    Console.WriteLine("Введите значение от 1 до 7");
+}
