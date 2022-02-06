497564604997a4a08f4261986cc6857f6459ebbf
SHOCK <beponuka666@gmail.com>
2022-02-06 17:10:46 +0500

    Added Task009

---
+++ Tasks/Task009/Program.cs
@@ -0,0 +1,8 @@
+﻿// 9. Показать последнюю цифру трёхзначного числа
+
+Console.WriteLine("Введите трёхзначное число");
+
+int number = int.Parse(Console.ReadLine());
+
+Console.Write("Последняя цифра введённого числа - ");
+Console.WriteLine(number % 10);
