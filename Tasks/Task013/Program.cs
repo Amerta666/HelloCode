dc66b6ef4222c2cb7a8014c221a43904beb0bfa8
SHOCK <beponuka666@gmail.com>
2022-02-06 18:32:14 +0500

    Added Task013

---
+++ Tasks/Task013/Program.cs
@@ -0,0 +1,22 @@
+﻿// 13. Выяснить, кратно ли число заданному, если нет, вывести остаток.
+using static System.Console;
+
+//int FirstNumber = new Random().Next(100, 1000);
+//WriteLine(FirstNumber);
+//int SecondNumber = new Random().Next(10, 100);
+//WriteLine(SecondNumber);
+//int result = FirstNumber / SecondNumber;
+
+//WriteLine(result); 
+
+
+int a1 = int.Parse(ReadLine()); 
+int a2 = int.Parse(ReadLine()); 
+WriteLine(GetAnswer(a1, a2));
+
+string GetAnswer(int a, int b)
+{   
+    if (a % b == 0) return $"Число {a} кратно {b}";
+    return $"Число {a} не кратно {b}, остаток - {a % b}";
+    //return (a % b).ToString();
+}
