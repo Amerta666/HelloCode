bc40f5e439ce2bcada3e45de842b86c5340dfaeb
SHOCK <beponuka666@gmail.com>
2022-02-06 18:57:38 +0500

    Added Task015

--- Tasks/Task015/Program.cs
+++ Tasks/Task015/Program.cs
@@ -1,1 +1,11 @@
 ﻿// 15. Дано число. Проверить кратно ли оно 7 и 23
+
+using static System.Console;
+
+WriteLine("Введите число");
+int Number = int.Parse(ReadLine());
+
+if (Number % 23 == 0) WriteLine (Number + " кратно 23");
+    else WriteLine (Number + " не кратно 23");
+if (Number % 7 == 0) WriteLine (Number + " кратно 7");
+    else WriteLine (Number + " не кратно 7");
