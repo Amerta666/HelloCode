6952738a586ce08ad9276c8ba4a4be0638dc8899
SHOCK <beponuka666@gmail.com>
2022-02-06 19:29:45 +0500

    Change code in Task013

--- Tasks/Task013/Program.cs
+++ Tasks/Task013/Program.cs
@@ -9,13 +9,28 @@ using static System.Console;
 
 //WriteLine(result); 
 
+// Проверка ввода числа (Если на вооде ввели не число например, а текст)
+int a1 = 0;
+int a2 = 0;
 
-int a1 = int.Parse(ReadLine()); 
-int a2 = int.Parse(ReadLine()); 
+if (!int.TryParse(ReadLine(), out a1))
+{
+    WriteLine("Ошибка ввода, введите число");
+    return;
+}
+
+if (!int.TryParse(ReadLine(), out a2))
+{
+    WriteLine("Ошибка ввода, введите число");
+    return;
+}
+
+//int a1 = int.Parse(ReadLine()); 
+//int a2 = int.Parse(ReadLine()); 
 WriteLine(GetAnswer(a1, a2));
 
 string GetAnswer(int a, int b)
-{   
+{
     if (a % b == 0) return $"Число {a} кратно {b}";
     return $"Число {a} не кратно {b}, остаток - {a % b}";
     //return (a % b).ToString();
