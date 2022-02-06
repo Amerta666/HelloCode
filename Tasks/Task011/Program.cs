5128eb14a862e8ec90d01a07903b4eeb3153466b
SHOCK <beponuka666@gmail.com>
2022-02-06 17:33:50 +0500

    Added Task011

---
+++ Tasks/Task011/Program.cs
@@ -0,0 +1,12 @@
+﻿// 11. Дано число из отрезка [10, 99]. Показать наибольшую цифру числа
+
+Console.WriteLine("Введите число от 10 до 99");
+int number = int.Parse(Console.ReadLine());
+int FirstNumber = number % 10;
+int SecondNumber = number / 10;
+
+int MaxNumber = FirstNumber;
+if (SecondNumber > FirstNumber) MaxNumber = SecondNumber;
+
+Console.Write("Наибольшая цифра числа - ");
+Console.WriteLine(MaxNumber);
