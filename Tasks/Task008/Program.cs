4c861eb2ffeaf5c55a1bd596cdec4ca9b60317d5
SHOCK <beponuka666@gmail.com>
2022-02-06 16:42:29 +0500

    Change code in Task008

--- Tasks/Task008/Program.cs
+++ Tasks/Task008/Program.cs
@@ -4,7 +4,7 @@ Console.WriteLine("Введите число");
 int N = int.Parse(Console.ReadLine());
 int number = 1;
 
-while (number < N)
+while (number <= N)
 {
     if (number % 2 == 0)
     Console.WriteLine(number); 
