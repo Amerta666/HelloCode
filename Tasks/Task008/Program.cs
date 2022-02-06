2cd126730e1bd601c375e672c0b1e21f172ff420
SHOCK <beponuka666@gmail.com>
2022-02-06 15:49:24 +0500

    Added Task008

---
+++ Tasks/Task008/Program.cs
@@ -0,0 +1,12 @@
+﻿// 8. Показать четные числа от 1 до N
+
+Console.WriteLine("Введите число");
+int N = int.Parse(Console.ReadLine());
+int number = 1;
+
+while (number < N)
+{
+    if (number % 2 == 0)
+    Console.WriteLine(number); 
+number++;
+}
