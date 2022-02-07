310c3fe321aa2d13152a647a5f77f2cc89be5b15
SHOCK <beponuka666@gmail.com>
2022-02-07 23:34:42 +0500

    Added Task021

---
+++ Tasks/Task021/Program.cs
@@ -0,0 +1,13 @@
+﻿// 21. Программа проверяет пятизначное число на палиндромом.
+
+using static System.Console;
+
+WriteLine("Введите пятизначное число");
+string Number = ReadLine();
+int FirstNumber = int.Parse(Number[0].ToString());
+int FifthNumber = int.Parse(Number[4].ToString());
+if (FirstNumber != FifthNumber) 
+{
+    WriteLine("Число не является палиндромом");
+}
+else WriteLine("Число является палиндромом");
