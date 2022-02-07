321df65f7e80b7225e31d030dc7dc4d62559d314
SHOCK <beponuka666@gmail.com>
2022-02-08 00:10:41 +0500

    Added Task023

---
+++ Tasks/Task023/Program.cs
@@ -0,0 +1,10 @@
+﻿// Показать таблицу квадратов чисел от 1 до N 
+
+using static System.Console;
+
+int N = int.Parse(ReadLine());
+
+for (int i = 1; i <= N; i++)
+{
+    WriteLine($"{i} * {i} = {i * i}"); 
+}
