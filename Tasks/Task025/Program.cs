cf4335d3d2dd31f5f8f86335f30207c7daa644a9
SHOCK <beponuka666@gmail.com>
2022-02-08 00:38:30 +0500

    Added Task025

---
+++ Tasks/Task025/Program.cs
@@ -0,0 +1,16 @@
+﻿// 25. Найти сумму чисел от 1 до А
+
+using static System.Console;
+
+int A = int.Parse(ReadLine());
+int sum = 1;
+for (int i = 2; i <= A; i++)
+{   
+    sum = i + sum;
+    WriteLine($"{sum - i} + {i} = {sum}");
+
+}
+if (A == 1)
+    {
+        WriteLine("1");
+    }
