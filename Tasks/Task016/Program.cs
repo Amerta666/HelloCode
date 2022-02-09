244a3583b0aa535cfff925d30bdd029860d6106b
SHOCK <beponuka666@gmail.com>
2022-02-09 19:32:09 +0500

    Added Task016

--- Tasks/Task016/Program.cs
+++ Tasks/Task016/Program.cs
@@ -1,1 +1,14 @@
 ﻿// Задача 16: Дано число обозначающее день недели. Выяснить является номер дня недели выходным
+
+using static System.Console;
+
+int NumberDay = int.Parse(ReadLine());
+WriteLine(GetAnswer(NumberDay));
+
+string GetAnswer(int a)
+{
+    if (a < 1) return $"Введённое число не является номером дня недели";
+    if (a <= 5) return $"Номер дня недели не является выходным";
+    if (a == 6 || a == 7) return $"Номер дня недели является выходным";
+    return $"Введённое число не является номером дня недели";
+}
