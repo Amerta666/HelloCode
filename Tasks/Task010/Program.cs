c4353029e7af440f115b0b281c8f1e05f0bba187
SHOCK <beponuka666@gmail.com>
2022-02-09 18:43:45 +0500

    Added Task010

---
+++ Tasks/Task010/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 10: Показать вторую цифру трёхзначного числа.
+
+using static System.Console;
+
+WriteLine("Введите трёзхначное число");
+//int number = int.Parse(ReadLine());
+
+//if (number > 999 || number < 100)
+//{
+//    WriteLine("Ошибка ввода, введите ТРЁХЗНАЧНОЕ ЧИСЛО !!!");
+//   return;
+//}
+
+//string Number = number.ToString();
+string Number = ReadLine();
+WriteLine(GetAnswer(Number));
+   
+string GetAnswer(string a)
+{
+    int SecondNumber = int.Parse(a[1].ToString());
+    return SecondNumber.ToString();
+}
