5e54bd5765097743befcc97e8a93ce62eee2445c
SHOCK <beponuka666@gmail.com>
2022-02-09 19:01:56 +0500

    Added Task012

--- Tasks/Task012/Program.cs
+++ Tasks/Task012/Program.cs
@@ -1,1 +1,16 @@
 ﻿// Задача 12: Удалить вторую цифру трёхзначного числа.
+
+using static System.Console;
+
+WriteLine("Введите трёхзначное число");
+string Number = ReadLine();
+
+WriteLine(GetAnswer(Number));
+   
+string GetAnswer(string a)
+{
+    int FirstNumber = int.Parse(a[0].ToString());
+    int ThirdNumber = int.Parse(a[2].ToString());
+    return FirstNumber.ToString() + ThirdNumber.ToString();
+    
+}
