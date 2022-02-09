ee26c7ced7bb9889a7118bb55ab8abe79e220d43
SHOCK <beponuka666@gmail.com>
2022-02-09 18:54:41 +0500

    Added Task014

--- Tasks/Task014/Program.cs
+++ Tasks/Task014/Program.cs
@@ -1,1 +1,21 @@
 ﻿// Задача 14: Найти третью цифру числа или сообщить, что её нет.
+
+using static System.Console;
+
+WriteLine("Введите число");
+int number = int.Parse(ReadLine());
+
+if (number < 100)
+{
+    WriteLine("Третьей цифры числа нет");
+    return;
+}
+
+string Number = number.ToString();
+WriteLine(GetAnswer(Number));
+   
+string GetAnswer(string a)
+{
+    int SecondNumber = int.Parse(a[2].ToString());
+    return SecondNumber.ToString();
+}
