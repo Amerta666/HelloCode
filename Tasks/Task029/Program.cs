6e9c04d0e3361f7d8440df86e7478e6c2aab63d4
SHOCK <beponuka666@gmail.com>
2022-02-10 23:25:10 +0500

    Added Task029

---
+++ Tasks/Task029/Program.cs
@@ -0,0 +1,12 @@
+﻿// Написать программу вычисления произведения чисел от 1 до N
+using static System.Console;
+
+WriteLine("Введите число");
+int N = int.Parse(ReadLine());  
+int sum = 1;              
+
+for (int i = 1; i <= N; i++)
+{                   
+    sum = i * sum;
+    WriteLine(sum);
+}
