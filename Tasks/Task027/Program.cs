323f0e61a200f3dd9bc1d20a0fc532f753ef6af5
SHOCK <beponuka666@gmail.com>
2022-02-10 23:13:15 +0500

    Change text Task027

--- Tasks/Task027/Program.cs
+++ Tasks/Task027/Program.cs
@@ -4,10 +4,10 @@ using static System.Console;
 
 string text = ReadLine();
 int count = 0;
-int number;
+int number = 0;
 
 for (int i = 0; i < text.Length; i++)
 {
     if (int.TryParse(text[i].ToString(), out number)) count++;
 }
-WriteLine($"Количество цифр {count}");
+WriteLine($"Количество цифр {count}"); //  WriteLine("Количество цифр " + count);
