52a4a03a5ce1d85b0c95a8d01d676fd646005fd7
SHOCK <beponuka666@gmail.com>
2022-02-25 18:21:29 +0500

    Task056 Task058 Task060 Task062

---
+++ Tasks/Task056/Program.cs
@@ -0,0 +1,40 @@
+﻿// 56. Написать программу, которая обменивает элементы первой строки и последней строки
+
+using static System.Console;
+
+
+Clear();
+
+WriteLine("Vvedite m");
+int m = int.Parse(ReadLine());
+WriteLine("Vvedite n");
+int n = int.Parse(ReadLine());
+
+int[,] matrix = new int[m, n];
+int temp = 0;
+
+for (int i = 0; i < matrix.GetLength(0); i++)
+{
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        matrix[i, j] = new Random().Next(100);
+        Write($"{matrix[i, j]} ");
+    }
+    WriteLine();
+}
+
+for (int j = 0; j < matrix.GetLength(1); j++)
+{
+    temp = matrix[0, j];
+    matrix[0, j] = matrix[n - 1, j];
+    matrix[n - 1, j] = temp;
+}
+
+for (int i = 0; i < matrix.GetLength(0); i++)
+{
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        Write($"{matrix[i, j]} ");
+    }
+    WriteLine();
+}
---
+++ Tasks/Task058/Program.cs
@@ -0,0 +1,44 @@
+﻿// 58. Написать программу, которая в двумерном массиве 
+// заменяет строки на столбцы или сообщить, что это невозможно 
+// (в случае, если матрица не квадратная).
+
+using static System.Console;
+
+
+Clear();
+
+WriteLine("Vvedite m");
+int m = int.Parse(ReadLine());
+WriteLine("Vvedite n");
+int n = int.Parse(ReadLine());
+
+if (m != n)
+{
+    WriteLine("Matriza ne kvadrataya");
+    return;
+}
+
+int[,] matrix = new int[m, n];
+int temp = 0;
+
+for (int i = 0; i < matrix.GetLength(0); i++)
+{
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        matrix[i, j] = new Random().Next(100);
+        Write($"{matrix[i, j]} ");
+    }
+    WriteLine();
+}
+
+for (int i = 0; i < matrix.GetLength(0); i++)
+{
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        temp =   matrix[i , j];
+        matrix[i,j] = matrix[j,i];
+        matrix[j,i] = temp;
+        Write($"{matrix[i, j]} ");
+    }
+    WriteLine();
+}
